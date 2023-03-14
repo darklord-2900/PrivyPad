34ef15ae9e547297d5d789de6a5c7cdefeaa85bc
Kshitij Gupta <84291907+darklord-2900@users.noreply.github.com>
2023-03-14 15:33:07 +0530

    video 49+ password ahshing done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,13 @@ const express=require('express');
 const router= express.Router();
 const User= require('../models/User')
 const { body, validationResult } = require('express-validator');
-router.post('/',[
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+const JWT_secret="harryisagoodB$oy";
+
+// create user using: POST "/api/auth/createuser" 
+router.post('/createuser',[
     body('email').isEmail(),
     body('name').isLength({ min: 5 }),
     body('password').isLength({ min: 5 }),
@@ -13,18 +19,35 @@ router.post('/',[
         return res.status(400).json({ errors: errors.array() });
         }
         // checking whether user with same email exists
-        let user=User.findOne({email:req.body.email})
-        if(user){
-            return req.status(400).json({error: "Sorry this email already exists"})
-        }
-        user=await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
+        try{
+            let user=await User.findOne({email:req.body.email})
+            console.log(user);
+            if(user){
+                return req.status(400).json({error: "Sorry this email already exists"});
+            }
+            const salt=await bcrypt.genSalt();
+            let secpass= await bcrypt.hash(req.body.password,salt);
+            //create a new user
+            user=await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: secpass ,
         })
-
-        // .then(user => res.json(user))
-        // .catch(err=>{console.log(err)
-        //     res.json({error: "please enter a unique value for email"})})
-})
+        const data={
+            user:{
+                id: user.id
+            }
+        }
+        const authtoken=jwt.sign(data,JWT_secret);
+        console.log(authtoken);
+        res.json({authtoken})    
+            // .then(user => res.json(user))
+            // .catch(err=>{console.log(err)
+            //     res.json({error: "please enter a unique value for email"})})
+        }
+        catch(error){
+            console.error(error.message)
+            res.status(500).send("some error occoured");
+        }
+    })
 module.exports=router
