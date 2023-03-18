c9067b8d74e9696ec6b5085e104beced63799466
Kshitij Gupta <84291907+darklord-2900@users.noreply.github.com>
2023-03-18 21:11:17 +0530

    update delete endpoint added

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const port = 5000
 app.use(express.json())
 // Available Routes 
 app.use('/api/auth',require('./routes/auth'))
-app.use('/api/auth',require('./routes/notes'))
+app.use('/api/notes',require('./routes/notes'))
 //create a user using: POST "/api/auth". Doesn't require auth
 app.get('/', (req, res) => {
     res.send('Hello World!')
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,21 +5,26 @@ const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 const { findOne } = require('../models/User');
- var fetchuser=require('../middleware/fetchuser');
+var fetchuser=require('../middleware/fetchuser');
 
 const JWT_secret="harryisagoodB$oy";
 
-//ROUTE 1: create user using: POST "/api/auth/createuser" 
+//==========================================================================//
+//      ROUTE 1: create user using: POST "/api/auth/createuser"             //
+//==========================================================================//
+
 router.post('/createuser',[
     body('email').isEmail(),
     body('name').isLength({ min: 5 }),
     body('password').isLength({ min: 5 }),
     ],async (req,res)=>{
+
         // if errors are present then bad request is returned
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
+
         // checking whether user with same email exists
         try{
             let user=await User.findOne({email:req.body.email})
@@ -29,7 +34,8 @@ router.post('/createuser',[
             }
             const salt=await bcrypt.genSalt();
             let secpass= await bcrypt.hash(req.body.password,salt);
-            //create a new user
+        
+            //creating a new user
             user=await User.create({
                 name: req.body.name,
                 email: req.body.email,
@@ -52,9 +58,12 @@ router.post('/createuser',[
         res.status(500).send("Internal server error ");
     }
 })
-//RROUTE 2: authenticate a user using: POST "/api/auth/login" No login required. 
+
+//======================================================================================//
+//RROUTE 2: authenticate a user using: POST "/api/auth/login" No login required.        //
+//======================================================================================//
 router.post('/login',[
-        body('email','enter valid email').isEmail(),
+    body('email','enter valid email').isEmail(),
         body('password','password cannot be blank').exists(),
     ],async (req,res)=>{
         const errors = validationResult(req);
@@ -84,7 +93,11 @@ router.post('/login',[
             res.status(500).send(" internal server error ");
         }
     })
-    // ROUTE 3:  Get details of a user using: POST "/api/auth/getuser" login required.
+    
+    //===================================================================================================//
+    //       ROUTE 3:  Get details of a user using: POST "/api/auth/getuser" login required.             //
+    //===================================================================================================//
+    
     router.post('/getuser', fetchuser ,async (req,res)=>{
     try{
         let userId=req.user.id;
