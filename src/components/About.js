dfe9652a65efa050563104d2f11b9da779e10d99
Kshitij Gupta <84291907+darklord-2900@users.noreply.github.com>
2023-04-05 15:12:59 +0530

    About Updated.

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,25 @@
 const About = () => {
     return (
         <div>
-        This is about page
+            <h4>
+                A simple Notes Management Application with features to store data provided by different users to keep their notes stored. 
+            </h4>
+            <h4>
+                iNotebook is made by following MERN Stack principles of Web Development.
+            </h4>
+            <h4>
+                The basic operations of this application are :
+            </h4>
+            <h5>
+                <ol>
+                    <li>
+                        Private and secure space for each user
+                    </li>
+                    <li>
+                    Users are able to Sign Up using their Name, Email, and can set a password which is securely stored in the database <br></br>in encrypted format to ensure safety to the user's data.
+                    </li>
+                </ol>
+            </h5>
         </div>
     )
 }
